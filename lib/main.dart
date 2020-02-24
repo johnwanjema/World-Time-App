aec82d51563e02079cf12890891bb3d32c513b04
john <jonwanjema@gmail.com>
2020-02-24 19:55:20 +0300

    initial commit with routes

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/screens/choose_loaction.dart';
+import 'screens/home.dart';
+import 'screens/loading.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/' : (context) => Loading(),
+    '/home' : (context) => Home(),
+    '/location' : (context) => ChooseLocation(),
+},
+));
+
+
