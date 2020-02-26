4bb54352d2e7dd18652fc797042fe8e36bc43c75
john <jonwanjema@gmail.com>
2020-02-26 20:29:49 +0300

    added try and catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,18 +11,28 @@ class worldTime {
   worldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    print(data);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
 
-    // create datatime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+    try {
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
 
-    //set time property
-    time = now.toString();
+      Map data = jsonDecode(response.body);
+      print(data);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+
+      // create datatime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      print(now);
+
+      //set time property
+      time = now.toString();
+    }catch(e ){
+      print('error $e.toString()');
+      time = "no data";
+
+    }
+
+
   }
 }
